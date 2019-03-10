049ccd2da8d64dc81f4862b7050ca923ae13b7cc
Alex Puga <alpuga8@gmail.com>
2019-03-10 16:56:13 -0400

    blockchain class

---
+++ blockchain.py
@@ -0,0 +1,15 @@
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    def new_block(self):
+        # Creates new block and adds to the chain pass
+
+    def new_transaction(self):
+        # Adds new transaction to the list of transactions pass
+
+
+@staticmethod
+def hash(block):
+    # Hashes a Block pass
